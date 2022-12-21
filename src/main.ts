8f862cbc1ad301e8b832e4c5c8822e3aebf53beb
Adriano Interaminense <adriano.interaminense@gmail.com>
2022-12-20 22:31:53 -0300

    Initialize vite and configure ts

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import RankingManager from "./ranking-manager";
+
+window["rankingManager"] = new RankingManager();
+
+export {};
---
+++ src/ranking-manager.ts
@@ -0,0 +1,21 @@
+class RankingManager {
+  constructor() {}
+
+  async createUser(userData: any) {
+    console.log("createUser");
+  }
+
+  async updateUser(userId: any, updatedData: any) {
+    console.log("updateUser");
+  }
+
+  async deleteUser(userId: any) {
+    console.log("deleteUser");
+  }
+
+  async getUser(userId: any) {
+    console.log("getUser");
+  }
+}
+
+export default RankingManager;
